62803dc96e4102d9b275089fdfcce38aea656eec
zzhai <amarsoft@>
2025-03-24 17:36:10 +0800

    模块初始化

---
+++ goods-order/src/main/java/com/ronin/GoodsOrderApplication.java
@@ -0,0 +1,12 @@
+package com.ronin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsOrderApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(GoodsOrderApplication.class, args);
+    }
+}
---
+++ goods-user/src/main/java/com/ronin/GoodsUserApplication.java
@@ -0,0 +1,14 @@
+package com.ronin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GoodsUserApplication
+{
+    public static void main( String[] args ) {
+
+        SpringApplication.run(GoodsUserApplication.class, args);
+    }
+}
